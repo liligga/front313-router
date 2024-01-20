057e83ab191a4223554fb1d319f23192b5ac7996
Igor <igglike@gmail.com>
2024-01-20 16:49:16 +0600

    Добавил фильтры с searchParams

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -1,33 +1,71 @@
-import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import axios from "axios"
-import { jsonplaceholderApi } from "../api"
+import { useState, useEffect } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import { jsonplaceholderApi } from "../api";
 
 const PostsPage = () => {
-    const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    useEffect(() => {
-        // fetch('https://jsonplaceholder.typicode.com/posts')
-        //     .then(response => response.json())
-        //     .then(data => setPosts(data))
-        const getPosts = async() => {
-            // const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-            const response = await jsonplaceholderApi.get('/posts')
-            setPosts(response.data)
-        }
-        getPosts()
-    }, [])
+  useEffect(() => {
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    //     .then(response => response.json())
+    //     .then(data => setPosts(data))
+    let url
+    const userId = searchParams.get("user");
+    if (userId) {
+      url = `/posts?userId=${userId}`;
+    } else {
+      url = "/posts";
+    }
+    const getPosts = async () => {
+      const response = await jsonplaceholderApi.get(url);
+      setPosts(response.data);
+    };
+    getPosts();
+  }, []);
+
+  const changeUserFilter = (userId) => {
+    console.log(userId);
+    setSearchParams({ user: userId });
+    const getPosts = async () => {
+        const response = await jsonplaceholderApi.get("/posts?userId=" + userId);
+        setPosts(response.data);
+      };
+    getPosts();
+  };
+
+  const resetUserFilter = () => {
+    setSearchParams({});
+    const getPosts = async () => {
+        const response = await jsonplaceholderApi.get("/posts");
+        setPosts(response.data);
+      };
+    getPosts();
+  };
 
   return (
     <div>
-        <ul>
-            {posts && posts.map(post => (
-                <li key={post.id}>
-                    <Link to={`/posts/${post.id}`}>{post.title}</Link>
-                </li>
-            ))}
-        </ul>
+      <div className="userFilter">
+        <button onClick={resetUserFilter}>Все посты</button>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((userId) => (
+          <button key={userId} onClick={() => changeUserFilter(userId)}>
+            User:{userId}
+          </button>
+        ))}
+      </div>
+      <ul>
+        {posts &&
+          posts.map((post) => (
+            <li key={post.id}>
+              <Link to={`/posts/${post.id}`}>{post.title}</Link>
+            </li>
+          ))}
+      </ul>
     </div>
-  )
-}
-export default PostsPage
+  );
+};
+export default PostsPage;
+
+// ?   sort=popular&
+//     page=1&
+//     fbrand=65084
